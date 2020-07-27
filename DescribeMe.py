8d2e5896e0890d6091d35c4877e2f5a19ff49a30
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-27 15:03:59 -0400

    Got rid of "main" function

--- DescribeMe.py
+++ DescribeMe.py
@@ -5,51 +5,55 @@ from face_detect import detect_faces
 from eyeglass_detector import detect_eyeglasses
 from PhotoAgeGender import guessAgeGender
 
-def get_image():
-    root = Tk()
-    root.title("CV Grammar")
-    root.geometry("300x180+500+250")
+root = Tk()
+root.title("CV Grammar")
+root.geometry("300x180+500+250")
 
-    Label(root, text="Enter the file location:", font=("time new roman",10)).place(x=20,y=50)
+Label(root, text="Enter the file location:", font=("time new roman",10)).place(x=20,y=50)
+
+file=Entry(root)
+
+def describe(path):
+    y_val = 50
     
-    file=Entry(root)
+    move_down = lambda y: y + 25
+    
+    glasses = detect_eyeglasses(path)
+    
+    #gets age and gender of person in the image (age first, gender last)
+    guess = guessAgeGender(path)
 
-    def describe(path):
-        y_val = 50
+    img = cv2.imread(path)
 
-        move_down = lambda y: y + 25
-        
-        glasses = detect_eyeglasses(path)
-        #gets age and gender of person in the image (age first, gender last)
-        guess = guessAgeGender(path)
-
-        img = cv2.imread(path)
-        
-        cv2.putText(img, "Age: " + str(guess[0]), (10, y_val), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+    if cv2.cvGetWindowHandle("Describe Me") != 0:
+        # Close all cv2 windows
+        cv2.destroyAllWindows()
+    
+    cv2.putText(img, "Age: " + str(guess[0]), (10, y_val), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+    y_val = move_down(y_val)
+    cv2.putText(img, "Gender: " + str(guess[1]), (10, y_val), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+    y_val = move_down(y_val)
+    if glasses:
+        cv2.putText(img, "With Glasses", (10, y_val), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
         y_val = move_down(y_val)
-        cv2.putText(img, "Gender: " + str(guess[1]), (10, y_val), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
-        y_val = move_down(y_val)
-        if glasses:
-            cv2.putText(img, "With Glasses", (10, y_val), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
-            y_val = move_down(y_val)
-        cv2.imshow("Describe Me", img)
 
-    def face_check():
-        try:
-            path = r""+str(file.get())
-            face_count = detect_faces(path)
-            if face_count > 1:
-                Label(root, text="* Only one face can be described.", font=("time new roman",10), fg='red').place(x=20,y=20)
-            else:
-                describe(path)
-            
-        except FileNotFoundError:  Label(root, text="* Please use an existing file.", font=("time new roman",10), fg='red').place(x=20,y=20)
-        except OSError:  Label(root, text="* No quotation marks in the file location.", font=("time new roman",10), fg='red').place(x=20,y=80)
-        #except: Label(root, text="* Please use an existing file.", font=("time new roman",10), fg='red').place(x=20,y=20)
-    file.place(x=150,y=50)
-     
-    Button(root, text='Describe Me',width=10, command=face_check).place(x=110,y=130)
+    cv2.imshow("Describe Me", img)
 
-    mainloop()
+def face_check():
+    try:
+        path = r""+str(file.get())
+        face_count = detect_faces(path)
+        if face_count > 1:
+            Label(root, text="* Only one face can be described.", font=("time new roman",10), fg='red').place(x=20,y=20)
+        else:
+            describe(path)
+        
+    except FileNotFoundError:  Label(root, text="* Please use an existing file.", font=("time new roman",10), fg='red').place(x=20,y=20)
+    except OSError:  Label(root, text="* No quotation marks in the file location.", font=("time new roman",10), fg='red').place(x=20,y=80)
+    except: Label(root, text="* Please use an existing file.", font=("time new roman",10), fg='red').place(x=20,y=20)
 
-get_image()
+file.place(x=150,y=50)
+ 
+Button(root, text='Describe Me',width=10, command=face_check).place(x=110,y=130)
+
+mainloop()
