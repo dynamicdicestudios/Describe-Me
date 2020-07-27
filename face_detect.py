97175c8e58b99ad3f2af099da9e09d092b6c3038
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-27 14:34:05 -0400

    Returns num of faces

--- face_detect.py
+++ face_detect.py
@@ -1,31 +1,33 @@
 import cv2
-import sys
 
-# Get user supplied values
-#imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
+def detect_faces(path):
+    # Get user supplied values
+    #imagePath = sys.argv[1]
+    cascPath = "haarcascade_frontalface_default.xml"
 
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Read the image
-image = cv2.imread(r"C:\Users\Josiah\Documents\GitHub\Describe-Me\abba.png")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Read the image
+    image = cv2.imread(path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
-)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
 
-print("Found {0} faces!".format(len(faces)))
+    return len(faces)
 
+"""
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
+"""
