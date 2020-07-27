043dc059850ada758cb2e9f52a79f37ad233daa2
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-27 14:34:20 -0400

    Puts age and gender in tuple

--- PhotoAgeGender.py
+++ PhotoAgeGender.py
@@ -22,53 +22,56 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
     return frameOpencvDnn, bboxes
 
-faceProto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
+def guessAgeGender(path):
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
 
-ageProto = "age_deploy.prototxt"
-ageModel = "age_net.caffemodel"
+    ageProto = "age_deploy.prototxt"
+    ageModel = "age_net.caffemodel"
 
-genderProto = "gender_deploy.prototxt"
-genderModel = "gender_net.caffemodel"
+    genderProto = "gender_deploy.prototxt"
+    genderModel = "gender_net.caffemodel"
 
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList = ['Male', 'Female']
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    genderList = ['Male', 'Female']
 
-# Load network
-ageNet = cv.dnn.readNet(ageModel, ageProto)
-genderNet = cv.dnn.readNet(genderModel, genderProto)
-faceNet = cv.dnn.readNet(faceModel, faceProto)
+    # Load network
+    ageNet = cv.dnn.readNet(ageModel, ageProto)
+    genderNet = cv.dnn.readNet(genderModel, genderProto)
+    faceNet = cv.dnn.readNet(faceModel, faceProto)
 
-# Open a video file or an image file or a camera stream
-frame = cv.imread("abba.png")
-padding = 20
-# Read frame
-t = time.time()
+    # Open a video file or an image file or a camera stream
+    frame = cv.imread(path)
+    padding = 20
+    # Read frame
+    t = time.time()
 
-frameFace, bboxes = getFaceBox(faceNet, frame)
-if not bboxes:
-    print("No face Detected, Checking next frame")
+    frameFace, bboxes = getFaceBox(faceNet, frame)
+    if not bboxes:
+        print("No face Detected, Checking next frame")
 
-for bbox in bboxes:
-    # print(bbox)
-    face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+    for bbox in bboxes:
+        # print(bbox)
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
 
-    blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-    genderNet.setInput(blob)
-    genderPreds = genderNet.forward()
-    gender = genderList[genderPreds[0].argmax()]
-    # print("Gender Output : {}".format(genderPreds))
-    print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        # print("Gender Output : {}".format(genderPreds))
+        print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
 
-    ageNet.setInput(blob)
-    agePreds = ageNet.forward()
-    age = ageList[agePreds[0].argmax()]
-    print("Age Output : {}".format(agePreds))
-    print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        print("Age Output : {}".format(agePreds))
+        print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
 
-    label = "{},{}".format(gender, age)
+        return (age, gender)
+
+    """label = "{},{}".format(gender, age)
     cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
     cv.imshow("Age Gender Demo", frameFace)
     # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
-print("time : {:.3f}".format(time.time() - t))
+print("time : {:.3f}".format(time.time() - t))"""
