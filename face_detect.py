abd009acbbbecaa5109349f2e71d55d3049092c3
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-31 12:43:23 -0400

    Trying to find skin colour

--- face_detect.py
+++ face_detect.py
@@ -1,13 +1,13 @@
 import cv2
 
+# Get user supplied values
+#imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
 def detect_faces(path):
-    # Get user supplied values
-    #imagePath = sys.argv[1]
-    cascPath = "haarcascade_frontalface_default.xml"
-
-    # Create the haar cascade
-    faceCascade = cv2.CascadeClassifier(cascPath)
-
     # Read the image
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -23,11 +23,77 @@ def detect_faces(path):
 
     return len(faces)
 
-"""
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+def find_bg(x1, y1, x2, y2, image):
+    colors = {}
+    colour = []
+    temp = 0
+    key = ""
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
-"""
+    area = (x2-x1) * (y2-y1)
+    half = area//2
+    print(area)
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            if str(image[i, j]) in colors:
+                color = str(image[i, j])
+                colors[color] += 1
+                
+                if temp < colors[color]:
+                    temp = colors[color]
+                    key = color
+                    colour = image[i, j]
+                if colors[color] == half:
+                    temp = colors[color]
+                    key = color
+                    colour = image[i, j]
+                    break         
+            elif str(image[i, j]) not in colors:
+                color = str(image[i, j])
+                colors[color] = 1
+
+    if temp == 0:
+            for color in colors:
+                if temp < colors[color]:
+                    temp = colors[color]
+                    key = color
+
+    return colour
+
+def face_colour(path):
+    print("Read the image")
+    image = cv2.imread(path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    print("Detect faces in the image")
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    print("Get x and y values")
+    
+    array = faces[0]
+    print(array)
+    
+    x, y, w, h = array
+    #cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    x1, y1, x2, y2 = x, y, x+w, y+h
+    print("Find face colour")
+    print(find_bg(x1, y1, x2, y2, image))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow("Faces found", image)
+    cv2.waitKey(0)
+
+face_colour("face.png")
+
+
+
+
+
